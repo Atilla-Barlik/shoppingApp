d54e0c2417ecb85289dd884fb18e514060846d9d
unknown <gurkanozgel58@gmail.com>
2022-04-23 01:04:36 +0300

    AnaSayfa Olusturuldu...

---
+++ OOP/MobileGUI/MobileGUI/Models/ProductItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileGUI.Models
+{
+    public class ProductItem
+    {
+        private string image;
+        public string ImageSource
+        { get { return image; } set { image = value; } }
+
+        private string product;
+        public string Product
+        { get { return product; } set { product = value; } }
+
+        private string id;
+        public string ID
+        { get { return id; } set { id = value; } }
+
+
+    }
+}
---
+++ OOP/MobileGUI/MobileGUI/ViewModels/Model.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileGUI.ViewModels
+{
+    internal class Model
+    {
+    }
+}
